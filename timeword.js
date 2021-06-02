7ed494993ae3e02aa70830c9fd14e84c810d7206
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-02 15:10:30 -0400

    Update timeword.js

--- timeword.js
+++ timeword.js
@@ -30,3 +30,29 @@ var lowWords = [
 var tensWords = ["twenty", "thirty", "forty", "fifty"];
 
 
+function timeToWords(timeStr) {
+  let hours = timeStr.split(":")[0];
+  let minutes = timeStr.split(":")[1];
+  if (hours == "00" && minutes == "00") {
+    return "midnight";
+  } else if (hours == "12" && minutes == "00") {
+    return "noon";
+  } else if (hours < "12") {
+    return `${convertHoursToWord(hours)} ${convertMinutesToWords(minutes)} am`;
+  } else {
+    return `${convertHoursToWord(hours)} ${convertMinutesToWords(minutes)} pm`;
+  }
+}
+
+function convertHoursToWord(hours) {
+  var result;
+  if (hours > "12") {
+    hours -= 12;
+  }
+  if (hours == "00") {
+    result = "twelve";
+  } else {
+    result = lowWords[parseInt(hours)];
+  }
+  return result;
+}
