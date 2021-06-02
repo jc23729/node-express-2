a9a9cf110a8e4cf04a53caf01d4617c03d1828ad
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-06-02 15:09:43 -0400

    Create timeword.js

---
+++ timeword.js
@@ -0,0 +1,32 @@
+// Solve the following problem in JavaScript:
+
+// Turn a string of 24h time into words.
+
+// You can trust that you’ll be given a valid string (it will always have a two-digit hour 00-23, and a two-digit minute 00-59). Hours 0-11 are am, and hours 12-23 are pm.
+
+var lowWords = [
+  "oh",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
+
+var tensWords = ["twenty", "thirty", "forty", "fifty"];
+
+
